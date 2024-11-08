a5a63d2fa7cdbed439064e08286bacf865460a4e
Taiga Saito <me@taigasaito.org>
2024-11-08 23:45:12 +0900

    update

--- app/(default)/quiz/[id]/page.tsx
+++ app/(default)/quiz/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { db } from '@/database';
 import { notFound } from 'next/navigation';
+import { FaArrowTrendUp } from 'react-icons/fa6';
 
 export const revalidate = 60;
 
@@ -14,16 +15,21 @@ export default async function QuizDetailPage({
     .where('quiz_id', '=', id)
     .leftJoin('users', 'quizzes.created_by', 'users.user_id')
     .selectAll()
-    .executeTakeFirst();
+    .executeTakeFirst()
+    .catch(() => null);
 
   if (!quizData) {
     return notFound();
   }
 
-  console.log('quizData', quizData);
-
   return (
     <>
+      <div className="flex h-[30svh] w-full items-center justify-center rounded-md bg-green-100">
+        <p className="text-3xl font-bold">{quizData.title}</p>
+      </div>
+
+      <h1 className="mt-5 text-4xl font-bold">{quizData.title}</h1>
+
       <p>
         QuizDetailPage
         <br />
@@ -46,6 +52,14 @@ export default async function QuizDetailPage({
         <br />
         Prompt: {quizData.prompt}
       </p>
+
+      <div className="w-full rounded-lg bg-blue-50">
+        <div className="flex gap-x-2">
+          <FaArrowTrendUp className="size-6 text-blue-500" />
+          <h2>自分のデータ</h2>
+        </div>
+        <span>ログインすると、自分のプレイデータを保存できます</span>
+      </div>
 
       <Link href={`/quiz/${id}/play`} className="btn">
         Play
