a997cbfa35dbbb6d293ab6ff8aed666dd980562b
Taiga Saito <me@taigasaito.org>
2024-12-09 22:53:34 +0900

    fix: init atom

--- libs/atoms.ts
+++ libs/atoms.ts
@@ -2,19 +2,12 @@ import { PlayStateAtom, QuizAtom } from '@/interfaces/atom';
 import { atomWithReset } from 'jotai/utils';
 
 export const quizAtom = atomWithReset<QuizAtom>({
-  // quizId: null,
-  // title: null,
-  // mode: 'ready',
-  // score: 0,
-  // correctCount: 0,
-  // totalCount: 0,
-
-  quizId: '8064ae5f-689c-4547-8722-10b5bd14417f',
-  title: '情報系クイズ',
-  mode: 'result',
-  score: 550,
-  correctCount: 14,
-  totalCount: 16,
+  quizId: null,
+  title: null,
+  mode: 'ready',
+  score: 0,
+  correctCount: 0,
+  totalCount: 0,
 });
 
 export const playStateAtom = atomWithReset<PlayStateAtom>({
